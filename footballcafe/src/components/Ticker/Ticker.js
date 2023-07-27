a14d83cd9ff85573c6ca5529e2434c0f4a983dff
wwpearce <wwpearce@gmail.com>
2023-07-27 12:32:51 -0400

    Made a slight change to the ticker hack. Added an href for ticker links to shut up react.

--- footballcafe/src/components/Ticker/Ticker.js
+++ footballcafe/src/components/Ticker/Ticker.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import './Ticker.scss';
 
+const tickerDest = "https://www.footballcafefc.com"; // Define the variable
+
 function Ticker({ isMobile, backgroundLoaded }) {
     const [isPaused, setIsPaused] = useState(false);
 
@@ -12,31 +14,31 @@ function Ticker({ isMobile, backgroundLoaded }) {
       setIsPaused(false);
     }
   return (
-    <div className="Ticker" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    <div className={`Ticker ${isMobile ? 'mobile' : 'desktop'}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
       <div className={`Ticker__content ${isMobile ? 'mobile' : 'desktop'} ${isPaused ? 'paused' : ''} ${backgroundLoaded ? 'loaded' : ''}`}>
         <span>141 Chrystie St New York, NY</span>
         <span>&mdash;</span>
-        <span><a href="">TEXT 917 909 6649</a></span>
+        <span><a href={tickerDest}>TEXT 917 909 6649</a></span>
         <span>&mdash;</span>
         <span>141 Chrystie St New York, NY</span>
         <span>&mdash;</span>
-        <span><a href="">TEXT 917 909 6649</a></span>
+        <span><a href={tickerDest}>TEXT 917 909 6649</a></span>
         <span>&mdash;</span>
         <span>141 Chrystie St New York, NY</span>
         <span>&mdash;</span>
-        <span><a href="">TEXT 917 909 6649</a></span>
+        <span><a href={tickerDest}>TEXT 917 909 6649</a></span>
         <span>&mdash;</span>
         <span>141 Chrystie St New York, NY</span>
         <span>&mdash;</span>
-        <span><a href="">TEXT 917 909 6649</a></span>
+        <span><a href={tickerDest}>TEXT 917 909 6649</a></span>
         <span>&mdash;</span>
         <span>141 Chrystie St New York, NY</span>
         <span>&mdash;</span>
-        <span><a href="">TEXT 917 909 6649</a></span>
+        <span><a href={tickerDest}>TEXT 917 909 6649</a></span>
         <span>&mdash;</span>
         <span>141 Chrystie St New York, NY</span>
         <span>&mdash;</span>
-        <span><a href="">TEXT 917 909 6649</a></span>
+        <span><a href={tickerDest}>TEXT 917 909 6649</a></span>
       </div>
     </div>
   );
