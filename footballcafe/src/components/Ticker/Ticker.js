c5b58fb02ab9e4a36765f68e19c833280fec5fa9
Bill Pearce <wwpearce@gmail.com>
2023-08-03 13:13:40 -0400

    Edited ticker per designs. Slowed it down a little. Added favicon.

--- footballcafe/src/components/Ticker/Ticker.js
+++ footballcafe/src/components/Ticker/Ticker.js
@@ -1,44 +1,67 @@
 import React, { useState } from 'react';
 import './Ticker.scss';
 
-const tickerDest = "https://my.community.com/kylemartinosfootballcafe";
+const tickerDest =
+  'https://my.community.com/kylemartinosfootballcafe';
 
 function Ticker({ isMobile, backgroundLoaded }) {
-    const [isPaused, setIsPaused] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
 
-    function handleMouseEnter() {
-      setIsPaused(true);
-    }
-  
-    function handleMouseLeave() {
-      setIsPaused(false);
-    }
+  function handleMouseEnter() {
+    setIsPaused(true);
+  }
+
+  function handleMouseLeave() {
+    setIsPaused(false);
+  }
   return (
-    <div className={`Ticker ${isMobile ? 'mobile' : 'desktop'}`} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-      <div className={`Ticker__content ${isMobile ? 'mobile' : 'desktop'} ${isPaused ? 'paused' : ''} ${backgroundLoaded ? 'loaded' : ''}`}>
+    <div
+      className={`Ticker ${isMobile ? 'mobile' : 'desktop'}`}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        className={`Ticker__content ${
+          isMobile ? 'mobile' : 'desktop'
+        } ${isPaused ? 'paused' : ''} ${
+          backgroundLoaded ? 'loaded' : ''
+        }`}
+      >
         <span>141 Chrystie St New York, NY</span>
-        <span>&mdash;</span>
-        <span><a href={tickerDest}>TEXT 917 909 6649</a></span>
-        <span>&mdash;</span>
+        <span>&#47;&#47;&#47;</span>
+        <span>
+          <a href={tickerDest}>TEXT 917 909 6649</a>
+        </span>
+        <span>&#47;&#47;&#47;</span>
         <span>141 Chrystie St New York, NY</span>
-        <span>&mdash;</span>
-        <span><a href={tickerDest}>TEXT 917 909 6649</a></span>
-        <span>&mdash;</span>
+        <span>&#47;&#47;&#47;</span>
+        <span>
+          <a href={tickerDest}>TEXT 917 909 6649</a>
+        </span>
+        <span>&#47;&#47;&#47;</span>
         <span>141 Chrystie St New York, NY</span>
-        <span>&mdash;</span>
-        <span><a href={tickerDest}>TEXT 917 909 6649</a></span>
-        <span>&mdash;</span>
+        <span>&#47;&#47;&#47;</span>
+        <span>
+          <a href={tickerDest}>TEXT 917 909 6649</a>
+        </span>
+        <span>&#47;&#47;&#47;</span>
         <span>141 Chrystie St New York, NY</span>
-        <span>&mdash;</span>
-        <span><a href={tickerDest}>TEXT 917 909 6649</a></span>
-        <span>&mdash;</span>
+        <span>&#47;&#47;&#47;</span>
+        <span>
+          <a href={tickerDest}>TEXT 917 909 6649</a>
+        </span>
+        <span>&#47;&#47;&#47;</span>
         <span>141 Chrystie St New York, NY</span>
-        <span>&mdash;</span>
-        <span><a href={tickerDest}>TEXT 917 909 6649</a></span>
-        <span>&mdash;</span>
+        <span>&#47;&#47;&#47;</span>
+        <span>
+          <a href={tickerDest}>TEXT 917 909 6649</a>
+        </span>
+        <span>&#47;&#47;&#47;</span>
         <span>141 Chrystie St New York, NY</span>
-        <span>&mdash;</span>
-        <span><a href={tickerDest}>TEXT 917 909 6649</a></span>
+        <span>&#47;&#47;&#47;</span>
+        <span>
+          <a href={tickerDest}>TEXT 917 909 6649</a>
+        </span>
       </div>
     </div>
   );
