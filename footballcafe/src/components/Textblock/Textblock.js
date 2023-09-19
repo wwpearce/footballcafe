bec7b26af4d90657933bf701ae69a2d7f0ca08d2
Bill Pearce <wwpearce@gmail.com>
2023-09-19 15:21:49 -0400

    Added all the css from the site to individual pages in the shopify directory.

--- footballcafe/src/components/Textblock/Textblock.js
+++ footballcafe/src/components/Textblock/Textblock.js
@@ -1,17 +1,36 @@
 import './Textblock.scss';
+import { ReactComponent as InstagramIcon } from '../../svg/instagram-icon.svg';
+import { ReactComponent as TwiterIcon } from '../../svg/twitter-icon.svg';
 
-const Textblock = ({ isMobile, isOpen, textBlockContent, textBlockHeading, isDefault }) => {
+const Textblock = ({
+  isMobile,
+  isOpen,
+  textBlockContent,
+  textBlockHeading,
+  isDefault,
+}) => {
   // const HeadingTag = isDefault ? 'p' : 'h1';
 
   return (
-    <div className={`Textblock ${isMobile ? 'mobile' : 'desktop'} ${isOpen ? 'open' : ''}`}>
-        <div className="Textblock__content">
-            <p>
-            {isDefault ? textBlockContent : textBlockHeading}
-            </p>
-        </div>
+    <div
+      className={`Textblock ${isMobile ? 'mobile' : 'desktop'} ${
+        isOpen ? 'open' : ''
+      }`}
+    >
+      <div className="Textblock__content">
+        <p>{isDefault ? textBlockContent : textBlockHeading}</p>
+      </div>
+      <div className="link-container">
+        <a href="instagram.com">
+          <InstagramIcon className="icons" />
+        </a>
+        <a href="twitter.com">
+          <TwiterIcon className="icons" />
+        </a>
+        <a href="apple.com">contact</a>
+      </div>
     </div>
   );
-}
+};
 
 export default Textblock;
